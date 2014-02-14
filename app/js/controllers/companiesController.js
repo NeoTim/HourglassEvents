929d4c12087d83506a352f94b8a31af7b0b6543c
hisimagination <joel.design@icloud.com>
2014-02-14 13:08:20 -0600

    Update added MTS Ads Functionality

--- app/js/controllers/companiesController.js
+++ app/js/controllers/companiesController.js
@@ -14,6 +14,7 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
 		var fireBaseUrl = "https://hourglass-events.firebaseio.com/" + exhibitorUrl + "/contacts";
 		var exhibitorRef = new Firebase("https://hourglass-events.firebaseIO.com/" + exhibitorUrl);
 		
+            //exhibitorRef.child('page').set({Contacts: true, Packages: true, MTS: true, Notes: true, Todos: true, Partner: true});
 
 		  var contactRef = new Firebase(fireBaseUrl);
 		 $scope.exh = $firebase(exhibitorRef);
@@ -78,7 +79,9 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
 				
 				// creates new incremental record
 		
-
+            $scope.removePage = function(page){
+                  exhibitorRef.child('page').child(page).set(false);
+            }
 
 }])
 
@@ -113,7 +116,7 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
 
 .controller("companyNotes", [ '$location', '$firebase', '$scope', function($location, $firebase, $scope, syncData){
 
-	var companyUrl = $location.path();
+  var companyUrl = $location.path();
     var Noteref  = 'http://hourglass-events.firebaseIO.com/' + companyUrl + "/notes";
     $scope.notes = $firebase(new Firebase(Noteref));
 
@@ -134,7 +137,7 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
     $scope.moveDown = function(id, pos){
         var ref = new Firebase(Noteref + "/" + id);
         var x = pos +1;
-        	ref.child('position').set(x);
+          ref.child('position').set(x);
         console.log(id);
       
     }
@@ -149,6 +152,71 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
       var ref = new Firebase(Noteref);
       ref.child(index).remove();
     };
+
+
+
+  
+
+}])
+
+.controller("companyAds", [ '$location', '$firebase', '$scope', function($location, $firebase, $scope, syncData){
+
+	var companyUrl = $location.path();
+    var adsRef  = 'http://hourglass-events.firebaseIO.com/' + companyUrl + "/ads";
+    $scope.ads = $firebase(new Firebase(adsRef));
+
+      $scope.update = function(id, field, newval){
+          var ref = new Firebase(adsRef + "/" + id);
+          ref.child(field).set(newval);
+          console.log(id);
+        
+      }
+
+
+      $scope.updateAdDate = function(id, newval){
+          var ref = new Firebase(adsRef + "/" + id);
+          var dueDate = [];
+          if(newval == "March/April"){
+              dueDate = "Feb 10"
+          } else if(newval == "May/June") {
+              dueDate = "Apr 10"
+          } else if(newval == "July/Aug") {
+              dueDate = "June 10"
+          } else if(newval == "Sept/Oct") {
+              dueDate = "Aug 10"
+          } else if(newval == "Nov/Dec") {
+              dueDate = "Oct 10"
+          }
+
+          ref.child('adDate').set(newval);
+          ref.child('dueDate').set(dueDate);
+          console.log(ref);
+        
+      }
+
+      $scope.updateStatus = function(id, newval){
+            var color = [];
+            var ref = new Firebase(adsRef);
+            if(newval == 'reminded'){
+                  color = 'warning';
+            } else if(newval == 'received'){
+                  color = 'success';
+            } else if(newval == 'sent'){
+                  color = 'info'
+            }
+            ref.child(id).child('status').set(newval);
+            ref.child(id).child('color').set(color);
+      }
+
+      $scope.addAds = function(){
+        var ref = new Firebase(adsRef);
+         ref.push({body: " "});
+         
+      };
+      $scope.removeAds = function(index){
+        var ref = new Firebase(adsRef);
+        ref.child(index).remove();
+      };
 
 
 
@@ -198,3 +266,49 @@ angular.module('myApp.companies', ['firebase', 'ui.router'])
   
 
 }]);
+
+var ModalDemoCtrl = function ($scope, $modal, $log, $firebase, $location) {
+
+  var fb = new Firebase("http://hourglass-events.firebaseIO.com/" + $location.path());
+
+  $scope.items = ['Contacts', 'Packages', 'MTS', 'Notes', 'Todos', 'Partner'];
+
+  $scope.open = function () {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: ModalInstanceCtrl,
+      resolve: {
+        items: function () {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+      fb.child('page').child(selectedItem).set(true);
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+};
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
